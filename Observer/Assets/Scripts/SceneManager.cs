b44d382b7c55a08b8f6b698d28b76c6f422f6638
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2014-01-24 22:38:55 -0500

    added scene manager

---
+++ Observer/Assets/Scripts/SceneManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SceneManager : MonoBehaviour 
+{
+    #region Singleton
+
+    private static SceneManager _instance;
+
+    public static SceneManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindObjectOfType(typeof(SceneManager)) as SceneManager;
+
+                if (_instance == null)
+                {
+                    _instance = new GameObject("SceneManager Temporary Instance", typeof(SceneManager)).GetComponent<SceneManager>();
+                }
+
+                _instance.Init();
+            }
+
+            return _instance;
+        }
+    }
+
+    protected void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            _instance.Init();
+        }
+    }
+
+    #endregion
+
+    public int Karma { get; set; }
+
+    private void Init()
+    {
+    }
+}
