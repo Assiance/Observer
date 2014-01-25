cb73d5fee63ac30cf70e0d86137edfa312625d06
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2014-01-25 14:35:50 -0500

    added a mirror

--- Observer/Assets/Scripts/FreezeObjectsEvent.cs
+++ Observer/Assets/Scripts/FreezeObjectsEvent.cs
@@ -3,37 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 
-namespace Assets.Scripts
+public class FreezeObjectsEvent : MonoBehaviour
 {
-    public class FreezeObjectsEvent : MonoBehaviour
+    public List<Rigidbody> RigidBodies { get; set; }
+    public bool HasTouchedObject = false;
+
+    public void OnEnable()
     {
-        public List<Rigidbody> RigidBodies { get; set; }
+        RigidBodies = GameObject.FindObjectsOfType(typeof(Rigidbody)).Select(i => (Rigidbody)i).ToList();
 
-        public void OnEnable()
+        //KeyboardEventManager.Instance.RegisterKeyDown(KeyCode.A, OnFreeze);
+    }
+
+    public void Start()
+    {
+        foreach (var bodies in RigidBodies)
         {
-            RigidBodies = GameObject.FindObjectsOfType(typeof(Rigidbody)).Select(i => (Rigidbody)i).ToList();
-
-            KeyboardEventManager.Instance.RegisterKeyDown(KeyCode.A, OnFreeze);
+            bodies.isKinematic = true;
         }
 
-        public void Start()
+        StartCoroutine(FreezeEvent());
+    }
+
+    IEnumerator FreezeEvent()
+    {
+        foreach (var bodies in RigidBodies)
         {
-            foreach (var bodies in RigidBodies)
-            {
-                bodies.velocity = Vector3.zero;
-                bodies.rotation = new Quaternion(0,0,0,0);
-                bodies.angularVelocity = Vector3.zero;
-            }
+            bodies.isKinematic = false;
         }
 
-        public void OnFreeze(KeyCode key)
+        int count = 1;
+        yield return new WaitForSeconds(4);
+        
+        foreach (var bodies in RigidBodies)
         {
-            foreach (var bodies in RigidBodies)
-            {
-                bodies.velocity = Vector3.zero;
-                bodies.rotation = new Quaternion(0, 0, 0, 0);
-                bodies.angularVelocity = Vector3.zero;
-            }
+            Random.seed = count;
+            bodies.velocity = new Vector3(Random.Range(-5, 5), Random.Range(-5, 5), Random.Range(-5, 5));
+            print(bodies.velocity);
+            bodies.angularVelocity = new Vector3(Random.Range(-3, 3), Random.Range(-3, 3), Random.Range(-3, 3));
+            count++;
         }
     }
 }
+
