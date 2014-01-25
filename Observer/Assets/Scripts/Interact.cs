1a176fe667f87d7df6408c2cdc6033cc41a5682c
Armillary <sjwaddel@ncsu.edu>
2014-01-25 11:19:18 -0500

    Interact and readable

---
+++ Observer/Assets/Scripts/Interact.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.MyGenericScripts.IO;
+
+public class Interact : MonoBehaviour {
+
+	//Used to detect interaction through collision
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if ( KeyboardEventManager.InteractKeyPress && coll.gameObject.tag == "Interactable" )
+        {
+            if (coll.gameObject.GetComponent(typeof(Readable)) != null)
+            {
+                Readable contact = (Readable)coll.gameObject.GetComponent(typeof(Readable));
+                contact.interact();
+            }
+            
+        }
+    }
+}
