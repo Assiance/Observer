37315a2f58bc3d4d726a9394d6329bc617fe93ff
Bradford Foxworth-Hill <bradford.foxworthhill@gmail.com>
2014-01-26 15:16:18 -0500

    Last and Final

--- Observer/Assets/Scripts/TeleportNode.cs
+++ Observer/Assets/Scripts/TeleportNode.cs
@@ -1,13 +1,15 @@
-﻿using UnityEngine;
+﻿using System.Linq;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class TeleportNode : MonoBehaviour {
+public class TeleportNode : MonoBehaviour
+{
     @RequireComponent AudioSource;
-     
+
     //[HideInInspector]
     public Transform DestinationNode;
-    
+
     public Transform Player;
     public static List<string> NodeNames = new List<string>();
     public static List<Transform> Nodes = new List<Transform>();
@@ -18,33 +20,54 @@ public class TeleportNode : MonoBehaviour {
     //
     const int DELAY = 30;
     protected static int _Timer = 0;
-	
+
     // Use this for initialization
-	void Start () {
+    void Start()
+    {
         Nodes.Add(this.transform);
         NodeNames.Add(this.gameObject.name + NodeNames.Count.ToString());
         NodesRef.Add(this);
 
         speaker = this.GetComponent<AudioSource>();
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
         if (_Timer > 0) _Timer--;
     }
     void OnTriggerEnter()
     {
-        if (_Timer == 0 && DestinationNode !=null)
+        if (_Timer == 0 && DestinationNode != null)
         {
             Player.transform.position = DestinationNode.position;
             speaker.PlayOneShot(teleport);
             _Timer = DELAY;
+
+
+            var RigidBodies = GameObject.FindObjectsOfType(typeof(Rigidbody)).Select(i => (Rigidbody)i).ToList();
+
+            foreach (var bodies in RigidBodies)
+            {
+                bodies.isKinematic = false;
+                bodies.useGravity = false;
+                bodies.mass = 1f;
+            }
+
+            int count = 1;
+            foreach (var bodies in RigidBodies)
+            {
+                Random.seed = count;
+                bodies.velocity = new Vector3(Random.Range(-.3f, .2f), Random.Range(-.3f, .2f), Random.Range(-.3f, .2f));
+                bodies.angularVelocity = new Vector3(Random.Range(-.1f, .1f), Random.Range(-.1f, .1f), Random.Range(-.1f, .1f));
+                count++;
+            }
+
         }
     }
     public void DrawLinks()
     {
-        if(DestinationNode !=null)
-        Debug.DrawLine(transform.position, DestinationNode.position, Color.red);
+        if (DestinationNode != null)
+            Debug.DrawLine(transform.position, DestinationNode.position, Color.red);
     }
 }
